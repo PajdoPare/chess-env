2d45f451e9cffa138b18fcc24ccc008007fd6ad5
Pajalone <sekulicpavle990@gmail.com>
2026-03-15 23:47:53 +0100

    Dodao potez.cs i pravicu odigravanje poteza

--- ProjekatSahOOP/Board.cs
+++ ProjekatSahOOP/Board.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace ProjekatSahOOP
 {
-    
-  
+
+
     public class Board
     {
-        Piece[,] board = new Piece[8,8];
-        public Piece GetPiece(int R, int C) => board[R,C];
-        public Piece GetPiece(Kvadrat K) => board[K.Row,K.Col];
-        public void SetPiece(int R, int C, Piece piece) => board[R,C] = piece;
+        Piece[,] board = new Piece[8, 8];
+        public Piece GetPiece(int R, int C) => board[R, C];
+        public Piece GetPiece(Kvadrat K) => board[K.Row, K.Col];
+        public void SetPiece(int R, int C, Piece piece) => board[R, C] = piece;
         public void SetPiece(Kvadrat K, Piece piece) => board[K.Row, K.Col] = piece;
         public bool Unutar(int R, int C)
         {
@@ -21,14 +21,33 @@ namespace ProjekatSahOOP
         }
         public Kvadrat GdeKralj(bool beli)
         {
-            for(int i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++)
             {
-                for(int j = 0; j < 8; j++)
+                for (int j = 0; j < 8; j++)
                 {
-                    if (board[i,j] is Kralj k && k.beli == beli)return new Kvadrat(i, j);
+                    if (board[i, j] is Kralj k && k.beli == beli) return new Kvadrat(i, j);
                 }
             }
             throw new Exception("NEMA KRALJA!!!");
         }
-    }
+        public bool Napadnut(Kvadrat K, bool beli)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Piece p = board[i, j];
+                    if (p == null || p.beli != beli) continue;
+                    p.RacunajPoteze(this, new Kvadrat(i, j));
+                    if (p.Potezi.Contains(K)) return true;
+                }
+            }
+            return false;
+        }
+        public bool SAH(bool beli)
+        {
+            Kvadrat kralj = GdeKralj(beli);
+            return Napadnut(kralj, !beli);
+        }
+    } 
 }
---
+++ ProjekatSahOOP/Potez.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjekatSahOOP
+{
+    internal class Potez
+    {
+        public Kvadrat Polazno { get; set; }
+        public Kvadrat Odredisno { get; set; }
+        public Piece Promocija { get; set; } // null ako nije promocija
+        public bool Rokada { get; set; } 
+        public bool EnPassant { get; set; }
+        public Kvadrat EnPassantKV { get; set; } 
+        public Potez(Kvadrat a, Kvadrat b)
+        {
+            Polazno = a;
+            Odredisno = b;
+        }
+    }
+}
